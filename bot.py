615b52a3b12066f909f214fbd45666453ece29ca
Alastair7 <lekhidugtal@gmail.com>
2018-10-08 15:00:35 +0530

    check the basic working

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import tweepy
-
 consumer_key = ""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-auth = tweepy.OAuthHandler.(consumer_key,consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
+# import pdb; pdb.set_trace()
+api = tweepy.API(auth)
+# print(api.status_code)
+api.update_status(status="checking twitter bot update")
