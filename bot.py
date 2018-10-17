c33f9d3b9bca55065e6570622de2a37adf02cb87
Alastair7 <lekhidugtal@gmail.com>
2018-10-17 18:31:10 +0530

    lol experiment

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import tweepy
+import requests
 consumer_key = ""
 consumer_secret = ""
 access_token = ""
@@ -6,7 +7,10 @@ access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
+data1 = "Joke of the Day\n"
+data2 = requests.get('https://api.yomomma.info/').json()["joke"]
 # import pdb; pdb.set_trace()
 api = tweepy.API(auth)
+# print(data1+" "+data2)
 # print(api.status_code)
-api.update_status(status="checking twitter bot update")
+api.update_status(status=data1+data2)
